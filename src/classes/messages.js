368a3d440d9307e8efc16098c42ee8966a37b875
Amelia Rose Aldridge <amelia@sanarinomi.com>
2020-05-25 20:05:13 +0200

    Modified list onReact

--- src/classes/messages.js
+++ src/classes/messages.js
@@ -120,30 +120,42 @@ class ListMessage {
             this._index -= 1;
         return await this._onListUpdate(this._index);
     }; }
+    get Emotes() { return this._emotes; }
     get onReact() { return this._onReact; }
+    get onEnd() { return this._onEnd; }
+    set onEnd(func) { this._onEnd = func; }
     get onNext() { return async function() {
         if(!this._indexLimit || this._index !== this._indexLimit)
             this._index += 1;
         return await this._onListUpdate(this._index);
     }; }
+    get OnlyFireOnce() { return this._once; }
+    get FireOnce() { return this._once; }
 
-    constructor(onlistupdate, onreact = null, message = null, index = 0, indexLimit = null) {
+    constructor(onlistupdate, onreact = {func: null, emotes: null, fireonce: false}, message = null, index = 0, indexLimit = null) {
         this._message = message;
         this._collector = null;
         this._index = index;
         this._indexLimit;
         this._onListUpdate = onlistupdate;
-        this._onReact = onreact;
+        this._onReact = onreact.func;
+        this._emotes = onreact.emotes;
+        this._once = onreact.fireonce;
+        this._onEnd;
     }
 
     async messageInit() {
         await this._message.react("⬅");
+        if(this._emotes)
+            for (let i = 0; i < this._emotes.length; i++) {
+                await this._message.react(Number.isInteger(this._emotes[i]) ? this._message.client.emojis.get(this._emotes[i]) : this._emotes[i]);
+            }
         await this._message.react("➡");
 
-        this._collector = this._message.createReactionCollector(() => true, {idle: 20000, dispose: true});
+        this._collector = this._message.createReactionCollector(reaction => {return this._emotes && this._emotes.includes(reaction.emoji.id ? reaction.emoji.id : reaction.emoji.name) ? true : reaction.emoji.name === "⬅" || reaction.emoji.name === "➡" ? true : false ;}, {idle: 20000, dispose: true});
 
             this._collector.on("collect", async (reaction, user) => {
-                if(reaction.me && reaction.count < 2) return;                
+                if(reaction.me && reaction.count < 2) return;        
 
                 switch(reaction.emoji.name) {
                     case "⬅":
@@ -154,6 +166,8 @@ class ListMessage {
                         break;
                     default:
                         if(this._onReact) this.onReact(this, reaction, user, false);
+                        if(this.OnlyFireOnce)
+                            this._collector.stop("completed");
                         break;
                 }
             });
@@ -170,6 +184,11 @@ class ListMessage {
                         if(this._onReact) this.onReact(this, reaction, true);
                         break;
                 }
+            });
+
+            this._collector.on("end", async (col, reason) => {
+                if(reason !== "completed")
+                    this.onEnd ? this.onEnd(this, col, reason) : this._message.edit(this._message.content + "\n\n**This message has timed-out!**");
             });
     }
 

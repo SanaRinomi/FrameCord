48cc6d539b3a29285e5602cd884612abb2d7a1b3
Amelia Rose Aldridge <amelia@sanarinomi.com>
2020-06-08 22:02:21 +0200

    Command middleware implemented

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const {Client} = require("./src/controllers/client");
 const Nodes = require("./src/classes/nodes");
 const CommandClasses = require("./src/classes/command");
 const Messages = require("./src/classes/messages");
+const MiddlewareHandler = require("./src/classes/commMiddleware");
 
 module.exports = {
     Nodes,
     Client,
     ...Messages,
     ...CommandClasses,
-    discordjs: require("discord.js")
+    MiddlewareHandler
 };
